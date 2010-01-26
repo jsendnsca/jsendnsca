4ed588d5c75a7198840a76e998f9fbaa4c4f0af4
rajneeshpatel <rajneeshpatel@d719a9fe-b3ce-11dd-b408-c1c371648c19>
2010-01-26 07:58:21 +0000

    Issue 26: removed commons-io as only using single class IOUtils.java which has been reinstated

---
+++ trunk/jsendnsca/src/main/java/com/googlecode/jsendnsca/utils/IOUtils.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.jsendnsca.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Utility IO methods
+ *
+ * @author Raj.Patel
+ * @since 1.1.1
+ */
+public class IOUtils {
+
+	private IOUtils() {
+		// not to be constructed
+	}
+
+	/**
+	 * Unconditionally close an <code>InputStream</code>.
+	 * <p>
+	 * Equivalent to {@link InputStream#close()}, except any exceptions will be
+	 * ignored. This is typically used in finally blocks.
+	 *
+	 * @param input
+	 *            the InputStream to close, may be null or already closed
+	 */
+	public static void closeQuietly(InputStream input) {
+		try {
+			if (input != null) {
+				input.close();
+			}
+		} catch (IOException ignore) {
+		}
+	}
+
+	/**
+	 * Unconditionally close an <code>OutputStream</code>.
+	 * <p>
+	 * Equivalent to {@link OutputStream#close()}, except any exceptions will be
+	 * ignored. This is typically used in finally blocks.
+	 *
+	 * @param output
+	 *            the OutputStream to close, may be null or already closed
+	 */
+	public static void closeQuietly(OutputStream output) {
+		try {
+			if (output != null) {
+				output.close();
+			}
+		} catch (IOException ignore) {
+		}
+	}
+
+	/**
+	 * Unconditionally close a <code>Socket</code>.
+	 * <p>
+	 * Equivalent to {@link Socket#close()}, except any exceptions will be
+	 * ignored. This is typically used in finally blocks.
+	 *
+	 * @param socket
+	 *            the socket to close, may be null or already closed
+	 */
+	public static void closeQuietly(Socket socket) {
+		try {
+			if (socket != null) {
+				socket.close();
+			}
+		} catch (IOException ignore) {
+		}
+	}
+
+	public static void closeQuietly(ServerSocket serverSocket) {
+		try {
+			if (serverSocket != null) {
+				serverSocket.close();
+			}
+		} catch (IOException ignore) {
+		}
+	}
+}
