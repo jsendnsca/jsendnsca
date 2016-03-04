0ea7656afc2853f49a438ea01bbe74987ef3d81f
Max Schwaab <max.schwaab@meteogroup.com>
2016-03-04 14:21:37 +0100

    Blowfish Encryptor
    Added BlowfishEncryptor implementation with an internal check for key length respecting JCE activation.

---
+++ src/main/java/com/googlecode/jsendnsca/encryption/BlowfishEncryptor.java
@@ -0,0 +1,91 @@
+package com.googlecode.jsendnsca.encryption;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+
+import static java.lang.System.arraycopy;
+import static javax.crypto.Cipher.ENCRYPT_MODE;
+
+public class BlowfishEncryptor implements Encryptor {
+
+    private static final String BLOWFISH = "Blowfish";
+    private static final String BLOWFISH_TRANSFORMATION = "Blowfish/CFB8/NoPadding";
+    private static final int INIT_VECTOR_BYTES_LENGTH = 8;
+
+    private int keyBytesLength;
+
+    public BlowfishEncryptor(final int keyBytesLength) {
+        assertValidKeyBytesLength(keyBytesLength);
+
+        this.keyBytesLength = keyBytesLength;
+    }
+
+    @Override
+    public void encrypt(final byte[] passiveCheckBytes, final byte[] initVector, final String password) {
+        final byte[] passwordBytes = password.getBytes();
+
+        assertValidPasswordBytesLength(passwordBytes);
+
+        try {
+            final Cipher cipher = Cipher.getInstance(BLOWFISH_TRANSFORMATION);
+            final byte[] key = toFixedSizeByteArray(passwordBytes, keyBytesLength);
+            final byte[] initVectorBytes = toFixedSizeByteArray(initVector, INIT_VECTOR_BYTES_LENGTH);
+            final SecretKeySpec keySpec = new SecretKeySpec(key, BLOWFISH);
+            final IvParameterSpec ivParameterSpec = new IvParameterSpec(initVectorBytes);
+
+            cipher.init(ENCRYPT_MODE, keySpec, ivParameterSpec);
+
+            final byte[] cipherBytes = cipher.doFinal(passiveCheckBytes);
+
+            arraycopy(cipherBytes, 0, passiveCheckBytes, 0, cipherBytes.length);
+        } catch(final GeneralSecurityException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void assertValidKeyBytesLength(final int keyBytesLength) {
+        if(keyBytesLength < 1) {
+            throw new IllegalArgumentException("keyBytesLength must be greater than zero");
+        }
+        if(!isJceEnabled() && keyBytesLength > 16) {
+            throw new IllegalArgumentException("JCE is not enabled and keyBytesLength is longer than 16 byte (128 bit)");
+        }
+        if(isJceEnabled() && keyBytesLength > 56) {
+            throw new IllegalArgumentException("keyBytesLength is longer than 56 byte (448 bit)");
+        }
+    }
+
+    private boolean isJceEnabled() {
+        try {
+            return Cipher.getMaxAllowedKeyLength(BLOWFISH) == 2147483647;
+        } catch(final NoSuchAlgorithmException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void assertValidPasswordBytesLength(final byte[] passwordBytes) {
+        final int passwordBytesLength = passwordBytes.length;
+
+        if(passwordBytesLength > keyBytesLength) {
+            throw new IllegalArgumentException( "Key size " + passwordBytesLength + " is longer than the specified key size " + keyBytesLength);
+        }
+    }
+
+    private byte[] toFixedSizeByteArray(byte[] source, int fixedLength) {
+        byte[] result = new byte[fixedLength];
+
+        for(int i = 0; i < fixedLength && i < source.length; ++i) {
+            if(i < source.length) {
+                result[i] = source[i];
+            } else {
+                result[i] = 0;
+            }
+        }
+
+        return result;
+    }
+
+}
